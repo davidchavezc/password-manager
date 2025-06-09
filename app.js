328215c436eb897d8d497ff0be523439323663be
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2025-06-09 00:41:43 -0600

    Add database connection

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const pg = require('pg')
 const app = express()
 const port = 3000
+require('dotenv').config()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Password manager app listening on port ${port}`)
-})
+});
+
+const pool = new pg.Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT
+});
